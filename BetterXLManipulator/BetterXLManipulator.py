99da80c106cb2eea8f4fc5e37081fd46d6917956
Trey Sweeney <rcs0065@auburn.edu>
2023-06-27 10:14:36 -0500

    Initial upload

---
+++ BetterXLManipulator/BetterXLManipulator.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#Necessary imports for xlsx files
+import openpyxl as xl
+
+#Create object for file input
+wb = xl.load_workbook('Homewood-resi-addresses.xlsx')
+#ws = wb.active
+
+#Initialize values for openpyxl use
+ws = wb['Sheet1']
+
+while True:
+
+#Accept user input
+        street = input("Street name: ")
+        day = input("Day to append: ")
+
+#Initialize values for loop use
+        i = 2
+
+#Iterate through column and return corresponding day in adjacent row
+        for i in range (2,6300):
+        
+            if ws.cell(row=i, column=9).value == street or ws.cell(row=i, column=1).value == street or street + " ":
+                ws.cell(row=i, column=10).value = day
+     
+#Break while loop    
+        keepgoing = input("Continue? Y/n: ")
+        if keepgoing == "n":
+            break
+            
+
+#Save workbook
+wb.save("Homewood-resi-addresses-UPDATED.xlsx")
+
+
